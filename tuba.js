729c261ea1e80eb5734a9409b434f59368770c8b
Unknown <TAdcox009@west-mec.org>
2019-04-25 17:02:00 -0700

    Final commit
    Added a try and catch statement to prevent the user from adding negative numbers in the acres and months field. Final commit, ready for review.

--- tuba.js
+++ tuba.js
@@ -31,7 +31,25 @@ var acresBox = document.forms[0].acres;
 
 /* verify acres text box entry is a positive number */
 function verifyAcres() {
-    testFormCompleteness();
+    var validity = true;
+    var messageText = "";
+
+    try {
+        if (!(acresBox.value > 0)) {
+            throw "Please enter a number of acres greater than 0.";
+        }
+    } catch (message) {
+        validity = false;
+        messageText = message;
+        // remove erroneous entry from input box
+        acresBox.value = "";
+    } finally {
+        acresComplete = validity;
+        // remove former recommendation
+        messageElement.innerHTML = messageText;
+        messageHeadElement.innerHTML = "";
+        testFormCompleteness();
+    }
 }
 
 /* verify at least one crops checkbox is checked */
@@ -41,8 +59,26 @@ function verifyCrops() {
 
 /* verify months text box entry is between 1 and 12 */
 function verifyMonths() {
-    testFormCompleteness();
+    var validity = true;
+    var messageText = "";
+    try {
+        if (!(monthsBox.value >= 1 && monthsBox.value <= 12)) {
+            throw "Please enter a number of months between 1 and 12.";
+        }
+    } catch (message) {
+        validity = false;
+        messageText = message;
+        // remove erroneous entry from input box
+        monthsBox.value = "";
+    } finally {
+        monthsComplete = validity;
+        // remove former recommendation
+        messageElement.innerHTML = messageText;
+        messageHeadElement.innerHTML = "";
+        testFormCompleteness();
+    }
 }
+
 
 /* verify that a fuel option button is selected */
 function verifyFuel() {
